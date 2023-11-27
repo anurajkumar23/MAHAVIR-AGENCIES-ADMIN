093708a646b816069dd95bfb421014d0ccfa5134
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-11-27 19:18:03 +0530

    added storeswitcher

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -14,11 +14,7 @@ const SetupPage = () => {
     }
   }, [isOpen, onOpen]);
 
-  return (
-    <div className="p-4">
-      Root Page
-    </div>
-  )
+  return null;
 };
  
 export default SetupPage;
