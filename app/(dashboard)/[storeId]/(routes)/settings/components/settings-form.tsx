0e8185e266355bfca1adc7c46309857e80dffce1
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-11-29 12:07:20 +0530

    added origin api route url

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -12,16 +12,17 @@ import { useParams, useRouter } from "next/navigation";
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { 
-  Form, 
-  FormControl, 
-  FormField, 
-  FormItem, 
-  FormLabel, 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
   FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
 
 
 
@@ -30,8 +31,8 @@ import { AlertModal } from "@/components/modals/alert-modal";
 
 
 
-interface SettingsFormProps{
-    initialData: store;
+interface SettingsFormProps {
+  initialData: store;
 }
 
 const fromSchema = z.object({
@@ -41,7 +42,7 @@ const fromSchema = z.object({
 type SettingsFormValue = z.infer<typeof fromSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({
-    initialData
+  initialData
 }) => {
   const params = useParams();
   const router = useRouter();
@@ -53,7 +54,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     defaultValues: initialData
   });
 
-  const onSubmit = async(data: SettingsFormValue) =>{
+  const onSubmit = async (data: SettingsFormValue) => {
     try {
       setLoading(true);
       await axios.patch(`/api/stores/${params.storeId}`, data);
@@ -61,12 +62,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
       toast.success("Store updated.")
     } catch (error) {
       toast.error("Something went wrong.")
-    }finally{
+    } finally {
       setLoading(false);
     }
   };
 
-  const onDelete =  async() =>{
+  const onDelete = async () => {
     try {
       setLoading(true)
       await axios.delete(`/api/stores/${params.storeId}`);
@@ -75,7 +76,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
       toast.success("Store deleted.")
     } catch (error) {
       toast.error("Make sure you removed all products and categories first.")
-    }finally{
+    } finally {
       setLoading(false)
       setOpen(false)
     }
@@ -83,50 +84,54 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 
   return (
     <>
-    <AlertModal
-    isOpen={open}
-    onClose={() => setOpen(false)}
-    onConfirm={onDelete}
-    loading={loading}
-    />
-    <div className="flex items-center justify-between">
-      <Heading 
-      title= "Settings"
-      description= "Manage store preferences"
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
       />
-      <Button
-      disabled={loading}
-      variant="destructive"
-      size="icon"
-      onClick={() => setOpen(true)}
-      >
-      <Trash className="h-4 w-64"/>
-      </Button>
-    </div>
-     <Separator/>
-      <Form {...form}>
-        <form onSubmit = {form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-        <div className="grid grid-cols-3 gap-8">
-        <FormField
-        control={form.control}
-        name = "name"
-        render={({field}) =>(
-          <FormItem>
-           <FormLabel>Name</FormLabel>
-           <FormControl>
-            <Input disabled={loading} placeholder="Store name" {...field}/>
-           </FormControl>
-           <FormMessage/>
-          </FormItem>
-        )}
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Settings"
+          description="Manage store preferences"
         />
-        </div>
-        <Button disabled={loading} className="ml-auto" type="submit">
-          Save Changes
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+        >
+          <Trash className="h-4 w-64" />
         </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Store name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save Changes
+          </Button>
         </form>
-
       </Form>
+      <Separator />
+      <ApiAlert 
+      title="NEXT_PUBLIC_API_URL" 
+      description={`${origin}/api/${params.storeId}`} 
+      variant="public" />
     </>
   )
 }
