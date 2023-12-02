6976cb7cc29183203ca96a637578a73956e83c45
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-12-02 21:08:54 +0530

    Added colors to the store model.  (generated by blackbox.ai )

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -35,6 +35,11 @@ export function MainNav({
             active: pathname === `/${params.storeId}/sizes`,
         },
         {
+            href: `/${params.storeId}/colors`,
+            label: 'Colors',
+            active: pathname === `/${params.storeId}/colors`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'settings',
             active: pathname === `/${params.storeId}/settings`,
