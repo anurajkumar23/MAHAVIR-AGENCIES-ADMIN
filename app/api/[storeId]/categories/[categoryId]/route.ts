6f55ec9f12ca777e534f6ecd5d0cfb61ea6ca2c0
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-12-12 19:20:29 +0530

    added stripe

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -15,6 +15,9 @@ export async function GET(
     const category = await prismadb.category.findUnique({
       where: {
         id: params.categoryId
+      },
+      include:{
+        billboard: true,
       }
     });
   
